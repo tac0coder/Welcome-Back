f059ae9cf974ba9b63f0659c01a38ad830f5eb14
uglyfence <76257046+tac0coder@users.noreply.github.com>
2021-12-06 21:08:37 +0000

    Uyqwagsycgwuydasgcyqgsaxyzcgusagxzucy gsauxZFGXuysaftzFXtwfsatyx

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ function getRandomInt(max) {
 var images = {0:'/IMG_1.jpeg',1:'/IMG_2.gif',2:'/IMG_3.gif',3:'/IMG_4.gif',4:'/IMG_5.gif',5:'/IMG_6.gif'}
 
 document.body.innerHTML += '<img src=https://raw.githubusercontent.com/tac0coder/Welcome-Back/main/images'+images[getRandomInt(6)]+'></img>'
+
+
+function myToggleConfetti(){
+  toggleConfetti()
+  document.getElementsByTagName('canvas')[0].style = "position:absolute; left: 0; top: 0; z-index: -1;"
+}
